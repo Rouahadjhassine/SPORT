747333cb1d634a97c7edc14354da5155ded19d2e
saifyedes <saifyedes20@gmail.com>
2025-02-18 14:31:50 +0100

    Modification program.cs

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,11 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Ajout de CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular",
+        policy => policy.WithOrigins("http://localhost:4200")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
 
 var app = builder.Build();
 
@@ -17,6 +24,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Activation de CORS
+app.UseCors("AllowAngular");
 
 app.UseAuthorization();
 
